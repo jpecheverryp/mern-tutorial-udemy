77b3ef7779763c9eaf0d013aeacd966ac864fcbd
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-01-22 15:55:42 -0500

    inserted No users found message inside a card component

--- frontend/src/user/components/UserItem/UserItem.js
+++ frontend/src/user/components/UserItem/UserItem.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import './UserItem.css'
-
 import Avatar from '../../../shared/components/UIElements/Avatar'
 import Card from '../../../shared/components/UIElements/Card'
+
+import './UserItem.css'
 
 const UserItem = props => {
   return (
