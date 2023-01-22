6198042011486bf563d6a8b1a2f4839840ab4572
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-01-22 14:28:04 -0500

    created UsersList component where users will be able to see other users and added it to the Users page

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList/UsersList'
+
 const Users = () => {
-  return <h1>Users Works!</h1>
+
+  const USERS = [{
+    id: 'u1',
+    name: 'John',
+    image: 'https://randomuser.me/api/portraits/men/75.jpg',
+    places: 3
+  }]
+
+  return <UsersList items={USERS} />
 }
 
 export default Users
