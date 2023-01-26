6f2966286dd449b7db6ebe235c6632dd9cf63001
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-01-25 20:50:55 -0500

    add filter to users page to only show places by the route user

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
 import PlaceList from '../components/PlaceList'
 
 const DUMMY_PLACES = [
@@ -29,8 +31,10 @@ const DUMMY_PLACES = [
 ]
 
 const UserPlaces = () => {
+  const userId = useParams().userId
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
   return (
-    <PlaceList items={DUMMY_PLACES} />
+    <PlaceList items={loadedPlaces} />
   )
 }
 
