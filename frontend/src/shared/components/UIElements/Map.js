95e5d018bf4cb0d1b99e0dd710d3547c2b5a8346
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-01-30 17:55:38 -0500

    add visual map to site

--- frontend/src/shared/components/UIElements/Map.js
+++ frontend/src/shared/components/UIElements/Map.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 import './Map.css'
 
 const Map = props => {
+  const mapRef = useRef()
+  
+  const { center, zoom } = props
+
+  useEffect(() => {    
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom
+    })
+    
+    new window.google.maps.Marker({ position: center, map: map })
+  },[ center, zoom ])
+
+  
+
   return (
-    <div className={`map ${props.className}`} style={props.style}>
-      
-    </div>
+    <div ref={mapRef} className={`map ${props.className}`} style={props.style}></div>
   )
 }
 
