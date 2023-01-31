cd76ac018a20741b244e8ac13536c5691be35204
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-01-31 18:07:24 -0500

    changed input component so it can receive an initial value and validity

--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -24,9 +24,9 @@ const inputReducer = (state, action) => {
 const Input = props => {
 
   const [inputState, dispatch] = useReducer(inputReducer, { 
-    value: '', 
-    isValid: false,
-    isTouched: false
+    value: props.value || '', 
+    isTouched: false,
+    isValid: props.valid || false,
   })
 
   const { id, onInput } = props
