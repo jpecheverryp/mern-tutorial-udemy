4221bf80e78dbeb7fd617aff3eb622afe9da3bf9
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-01-31 19:52:17 -0500

    make sure that the form component only loads after the data is already loaded to the client

--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Button from '../../shared/components/FormElements/Button'
 import Input from '../../shared/components/FormElements/Input'
@@ -36,18 +36,32 @@ const DUMMY_PLACES = [
 const UpdatePlace = () => {
   const placeId = useParams().placeId
 
-  const identifiedPlace = DUMMY_PLACES.find(place => place.id === placeId)
-
-  const [formState, inputHandler] = useForm({
+  const [formState, inputHandler, setFormData] = useForm({
     title: {
-      value: identifiedPlace.title,
-      isValid: true
+      value: '',
+      isValid: false
     },
     description: {
-      value: identifiedPlace.description,
-      isValid: true
+      value: '',
+      isValid: false
     },
-  }, true)
+  }, false)
+
+  const identifiedPlace = DUMMY_PLACES.find(place => place.id === placeId)
+
+  useEffect(() => {
+    setFormData({
+      title: {
+        value: identifiedPlace.title,
+        isValid: true
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true
+      }
+    }, true)
+  }, [setFormData, identifiedPlace])
+
 
   const placeUpdateSubmitHandler = event => {
     event.preventDefault()
@@ -62,30 +76,40 @@ const UpdatePlace = () => {
     )
   }
 
-  return <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
-    <Input 
-      id='title'
-      element='input'
-      type='text'
-      label='Title'
-      validators={[VALIDATOR_REQUIRE()]}
-      errorText='Please enter a valid title.'
-      onInput={inputHandler}
-      value={formState.inputs.title.value}
-      valid={formState.inputs.title.isValid}
-    />
-    <Input 
-      id='description'
-      element='textarea'
-      label='Description'
-      validators={[VALIDATOR_MINLENGTH(5)]}
-      errorText='Please enter a valid description ( Min 5 characters ).'
-      onInput={inputHandler}
-      value={formState.inputs.description.value}
-      valid={formState.inputs.description.isValid}
-    />
-    <Button type='submit' disabled={!formState.isValid}>Update Place</Button>
-  </form>
+  if (!formState.inputs.title.value) {
+    return (
+      <div className='center'>
+        <h2>Loading...</h2>
+      </div>
+    )
+  }
+
+  return (
+      <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+        <Input 
+          id='title'
+          element='input'
+          type='text'
+          label='Title'
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText='Please enter a valid title.'
+          onInput={inputHandler}
+          value={formState.inputs.title.value}
+          valid={formState.inputs.title.isValid}
+        />
+        <Input 
+          id='description'
+          element='textarea'
+          label='Description'
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText='Please enter a valid description ( Min 5 characters ).'
+          onInput={inputHandler}
+          value={formState.inputs.description.value}
+          valid={formState.inputs.description.isValid}
+        />
+        <Button type='submit' disabled={!formState.isValid}>Update Place</Button>
+      </form>
+  )
 }
 
 export default UpdatePlace
