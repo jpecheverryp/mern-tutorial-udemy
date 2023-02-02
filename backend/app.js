f61122897a1369a2d38e782b66df9bbdb07e742a
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-02 18:36:45 -0500

    create backend folder and initialized express app

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const placesRoutes = require('./routes/places-routes');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }))
+app.use(placesRoutes)
+
+
+
+app.listen(5000)
