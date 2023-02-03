8ac56ad13b30a778c6608d0349392e42f1ce5c88
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-03 18:11:57 -0500

    add route to delete a place by id

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require('uuid');
 
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire State Building',
@@ -71,7 +71,11 @@ const updatePlace = (req, res, next) => {
   res.status(200).json({place: updatedPlace})
 }
 
-const deletePlace = (req, res, next) => {}
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid
+  DUMMY_PLACES = DUMMY_PLACES.filter(place => place.id !== placeId)
+  res.status(200).json({message: 'Deleted place succesfully.'})
+}
 
 exports.getPlaceById = getPlaceById
 exports.getPlaceByUserId = getPlaceByUserId
