3c2cbf29f71e8534f1b5d5eac3dd933c34d8bf75
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-03 19:53:29 -0500

    add validation on delete place to check if the place exists before trying to delete it

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -95,6 +95,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    return next(new HttpError('Could not find a place with the given id', 404))
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter(place => place.id !== placeId)
   res.status(200).json({message: 'Deleted place succesfully.'})
 }
