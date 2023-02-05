783102cbc46ba1105c076301bb70964fd087b584
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-05 16:32:23 -0500

    connected signup controller to mongodb

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,6 +1,8 @@
 const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
+
 const HttpError = require('../models/http-error');
+const User = require('../models/user');
 
 const DUMMY_USERS = [
   {
@@ -15,27 +17,39 @@ const getUsers = (req, res, neaxt) => {
   return res.json({ users: DUMMY_USERS })
 }
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     return next(new HttpError('Invalid inputs given, please check your data', 422))
   }
 
-  const {name, email, password} = req.body
+  const {name, email, password, places} = req.body
 
-  const hasUser = DUMMY_USERS.find(u => u.email === email)
-  if (hasUser) {
-    return next(new HttpError('Could not create user, email already registered', 422))
+  let existingUser
+  try {
+    existingUser = await User.findOne({ email: email })
+  } catch (err) {
+    return next( new HttpError('Signing Up Failed. Please try again later.', 500) )
+  }
+  if (existingUser) {
+    return next( new HttpError('Email already registered', 422) )
   }
 
-  const createdUser = {
-    id: uuidv4(),
-    name, 
+  const createdUser = new User({
+    name,
     email,
-    password
+    image: 'https://randomuser.me/api/portraits/men/75.jpg',
+    password,
+    places
+  })
+  
+  try {
+    await createdUser.save()
+  } catch (err) {
+    return next( new HttpError('Signing up failed. Please try again later', 500) )
   }
-  DUMMY_USERS.push(createdUser)
-  res.status(201).json({user: createdUser})
+
+  res.status(201).json({user: createdUser.toObject({ getters: true })})
 }
 
 const login = (req, res, next) => {
