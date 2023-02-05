5d336bd808ffa982adb5873b09d973d6d8ce4af2
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-05 15:53:50 -0500

    deleted DUMMY_PLACES from palces-controllers file because the controllers are connected to the mongo database now

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -5,30 +5,6 @@ const HttpError = require('../models/http-error');
 const getCoordinatesForAddress = require('../util/location');
 const Place = require('../models/place');
 
-let DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous skyscrappers in the world.',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Wall Street',
-    description: 'One of the most famous skyscrappers in the world.',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  },
-]
 
 const getPlacesById = async (req, res, next) => {
   
