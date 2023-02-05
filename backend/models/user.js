8fa0fbab06c495428facf5c88cb39a5aa0b1f276
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-05 16:46:32 -0500

    fixed schema, password was not a property of user and name was stated twice

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 const userSchema = Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  name: { type: String, required: true, minLength: 6 },
+  password: { type: String, required: true, minLength: 6 },
   image: { type: String, required: true },
   places: { type: String, required: true }
 })
