a8eb8fa4cdd2454327e07b8bb226ab75a5cfd95e
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-12 12:47:04 -0500

    show a modal when an error occurs on the signup page

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -15,7 +15,7 @@ const Auth = () => {
   const auth = useContext(AuthContext)
   const [isLoginMode, setIsLoginMode] = useState(true)
   const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState()
+  const [error, setError] = useState(null)
 
   const [formState, inputHandler, setFormData] = useForm({
     email: {
@@ -69,68 +69,77 @@ const Auth = () => {
         })
 
         const responseData = await response.json()
+        if (!response.ok) {
+          throw new Error(responseData.message)
+        }
         console.log(responseData);
         setIsLoading(false)
         auth.login()
-      } catch (err) {
-        console.error(err);
+      } catch (error) {
+        console.error(error);
         setIsLoading(false)
-        setError(err.message || 'Something went wrong, please try again')
+        setError(error.message || 'Something went wrong, please try again')
       }
     }
   }
 
+  const errorHandler = () => {
+    setError(null)
+  }
   return (
-    <Card className='authentication'> 
-      {isLoading && <LoadingSpinner asOverlay />} 
-      <h2>
-        {isLoginMode ? 
-          "Log In": 
-          "Sign Up"
-        }
-      </h2>
-      <hr />
-      <form onSubmit={authSubmitHandler}>
-        {!isLoginMode && (
-          <Input
-            element='input'
-            id='name'
-            type='text'
-            label='Your Name'
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText='Please enter a valid name.'
-            onInput={inputHandler}
-          />
-          )}
-        <Input
-          element='input' 
-          id='email'
-          type='email'
-          label='Email' 
-          validators={[VALIDATOR_EMAIL()]}
-          errorText='Please enter a valid email address.'
-          onInput={inputHandler}
-        />
-        <Input
-          element='input' 
-          id='password'
-          type='password'
-          label='Password' 
-          validators={[VALIDATOR_MINLENGTH(5)]} 
-          errorText='Please enter a valid password, at least 5 characters.'
-          onInput={inputHandler}
-        />
-        <Button type='submit' disabled={!formState.isValid}>
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className='authentication'> 
+        {isLoading && <LoadingSpinner asOverlay />} 
+        <h2>
           {isLoginMode ? 
             "Log In": 
             "Sign Up"
           }
+        </h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              element='input'
+              id='name'
+              type='text'
+              label='Your Name'
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText='Please enter a valid name.'
+              onInput={inputHandler}
+            />
+            )}
+          <Input
+            element='input' 
+            id='email'
+            type='email'
+            label='Email' 
+            validators={[VALIDATOR_EMAIL()]}
+            errorText='Please enter a valid email address.'
+            onInput={inputHandler}
+          />
+          <Input
+            element='input' 
+            id='password'
+            type='password'
+            label='Password' 
+            validators={[VALIDATOR_MINLENGTH(5)]} 
+            errorText='Please enter a valid password, at least 5 characters.'
+            onInput={inputHandler}
+          />
+          <Button type='submit' disabled={!formState.isValid}>
+            {isLoginMode ? 
+              "Log In": 
+              "Sign Up"
+            }
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          Switch to {isLoginMode ? 'Sign Up': 'Log In'}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        Switch to {isLoginMode ? 'Sign Up': 'Log In'}
-      </Button>
-    </Card>
+      </Card>
+    </>
   )
 }
 
