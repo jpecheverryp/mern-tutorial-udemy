43cd440e6baf24331aafb48ac261860e16575df5
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-12 14:34:45 -0500

    add fetch call to the login route

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -50,12 +50,34 @@ const Auth = () => {
 
   const authSubmitHandler = async event => {
     event.preventDefault()
+    setIsLoading(true)
     
     if (isLoginMode) {
-      // fetch('http://localhost:5000/api/users/login')
+      try {
+        const response = await fetch('http://localhost:5000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          })
+        })
+
+        const responseData = await response.json()
+        if (!response.ok) {
+          throw new Error(responseData.message)
+        }
+        setIsLoading(false)
+        auth.login()
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false)
+        setError(error.message || 'Something went wrong, please try again')
+      }
     } else {
       try {
-        setIsLoading(true)
         const response = await fetch('http://localhost:5000/api/users/signup', {
           method: 'POST',
           headers: {
@@ -72,7 +94,6 @@ const Auth = () => {
         if (!response.ok) {
           throw new Error(responseData.message)
         }
-        console.log(responseData);
         setIsLoading(false)
         auth.login()
       } catch (error) {
