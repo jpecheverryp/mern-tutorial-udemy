eacb173fc368501df4ba544bfad76e289b315e15
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-18 14:14:25 -0500

    add new ImageUpload component

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -4,6 +4,8 @@ import Input from '../../shared/components/FormElements/Input'
 import Card from '../../shared/components/UIElements/Card'
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import ImageUpload from '../../shared/components/FormElements/ImageUpload'
+
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators'
 
 import { useForm } from '../../shared/hooks/form-hook'
@@ -106,6 +108,9 @@ const Auth = () => {
               errorText='Please enter a valid name.'
               onInput={inputHandler}
             />
+            )}
+            {!isLoginMode && (
+              <ImageUpload center id='image' />
             )}
           <Input
             element='input' 
