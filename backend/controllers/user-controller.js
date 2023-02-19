822caad069715b953779410765d76957e59fdcfb
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2023-02-18 21:44:42 -0500

    create json web token when login in and signing up

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
 
@@ -54,7 +56,26 @@ const signup = async (req, res, next) => {
     return next( new HttpError('Signing up failed. Please try again later', 500) )
   }
 
-  res.status(201).json({user: createdUser.toObject({ getters: true })})
+  let token
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.email }, 
+      process.env.JWT_SECRET, 
+      { expiresIn: '1h' }
+    )
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later',
+      500
+    )
+    return next(error)
+  }
+
+  res.status(201).json({
+    userId: createdUser.id, 
+    email: createdUser.email, 
+    token 
+  })
 }
 
 const login = async (req, res, next) => {
@@ -86,9 +107,25 @@ const login = async (req, res, next) => {
     return next( new HttpError('Invalid password. Please try again', 401) )
   }
 
+  let token
+  try {
+    token = jwt.sign(
+      { userId: user.id, email: user.email }, 
+      process.env.JWT_SECRET, 
+      { expiresIn: '1h' }
+    )
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later',
+      500
+    )
+    return next(error)
+  }
+
   res.json({
-    message: 'Logged In!!', 
-    user: user.toObject({ getters: true })
+    userId: user.id,
+    email: user.email,
+    token: token
   })
 }
 
